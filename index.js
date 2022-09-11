38c063c850d9796458b89770ce389c5d7d766781
Benny Sufrin <bennysuf@gmail.com>
2022-09-11 14:38:02 -0400

    commit tree 636da29f4542be47af56776caaa0226cc49169b9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userName, emailAd){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+               },
+        body: JSON.stringify(
+            {
+          name: userName,
+          email: emailAd,
+    }),
+  })
+ .then((resp) => resp.json())
+ .then((data) => document.body.innerHTML = data.id ) //needs to return ID
+ .catch((error) => {
+   document.body.innerHTML = 'Unauthorized Access'
+ })
+ }
+submitData('benny', 'benny@gmail')
